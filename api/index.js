14c9d6218d1d5f3e6aa19fd4dbe2ec134fdd72c5
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-11-11 15:15:35 +0530

    app running

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const postRoute = require("./routes/posts");
 const catRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 dotenv.config();
 app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose
@@ -20,6 +20,11 @@ mongoose
     console.log("Error while connecting to Database");
   });
 
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", catRoute);
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
@@ -35,11 +40,14 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/user", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", catRoute);
-app.listen(5000, () => {
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
   console.log("Backend server running");
 });
